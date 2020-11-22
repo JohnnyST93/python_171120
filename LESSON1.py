e9667de4e32102692760787cc108285af0b66216
Johnny <evgeniy.statnikov@mail.ru>
2020-11-23 01:11:00 +0300

    hometask lesson1

---
+++ LESSON1.py
@@ -0,0 +1,54 @@
+#a = 12
+#b = 12
+#print (a,b)
+#c = int(input("Введите число:"))
+#d = input("Input your name: ")
+#print(c,d)
+
+#p = int(input("Введите время в секундах: "))
+#hours = p // 3600
+#minutes = (p // 60) - (hours * 60)
+#seconds = p % 60
+#print(f'{hours:02} : {minutes:02} : {seconds:02}')
+
+#a = input("Введите число: ")
+#b = a + a
+#c = b + a
+#res = int(a) + int(b) + int(c)
+#print (f"{a} + {b} + {c} = {res}")
+
+#a = int(input("Введите целое положительное число: "))
+#current_max = -1
+#num_rest = a
+#while num_rest > 0:
+#    last_num = num_rest % 10
+#    if last_num > current_max:
+#        current_max = last_num
+#        if current_max == 9:
+#                break
+#    else:
+#        current_max = current_max
+#        num_rest = num_rest // 10
+#print (f'Максимальная цифра в числе: {current_max}')
+
+#proceed = int(input("Введите значение выручки "))
+#costs = int(input("Введите значение издержек "))
+#if costs > proceed:
+#    print("Фирма отработала с убытком за данный период")
+#elif proceed > costs:
+#    print("Фирма отработала с прибылью за данный период")
+#    rent = (proceed - costs) / (proceed)
+#    sotr = int(input("Введите число сотрудников учреждения: "))
+#    profsotr = (proceed - costs) / sotr
+#    print (f'Рентабельность равна {rent}. Доля прибыли на одного сотрудника {profsotr}')
+
+#a = int(input("Изначальная длина дистанции "))
+#b = int(input("Планируемая к достижению дистанция "))
+#current_day = 0
+#while b > a:
+#    a = a + (a * 1 / 10)
+#    current_day = current_day + 1
+#print(f'Результат будет достигнут на {current_day + 1}-й день')
+
+
+
